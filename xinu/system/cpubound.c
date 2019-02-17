7b5b2014d90f9ebfd3c870c3e26cd14e0b73f322
parth-shel <ps@parthshel.com>
2019-02-17 05:45:30 -0500

    adding test simulation

---
+++ xinu/system/cpubound.c
@@ -0,0 +1,47 @@
+/* cpubound */
+
+#include <xinu.h>
+
+void cpubound() {
+    for (int i = 0; i < 150; i++) {
+        for (int j = 0; j < 150; j++) {
+            // calculate i^j
+            uint32 base = i, exponent = j;
+            long long result = 1;
+            while (exponent != 0) {
+                result *= base;
+                exponent--;
+            }
+
+            // calculate j^i
+            base = j, exponent = i;
+            result = 1;
+            while (exponent != 0) {
+                result *= base;
+                exponent--;
+            }
+
+            // calculate hailstone sequence
+            uint32 num = 300;
+            while(num != 1) {
+                if (num%2 == 0) {
+                    num /= 2;
+                } else {
+                    num = num*3+1;
+                }
+            }
+
+            // perform memcpy
+            char src[] = "hello world!";
+            char dest[15];
+            memcpy(dest, src, strlen(src)+1);
+        }
+    }
+
+    kprintf("CPU bound process.\n");
+
+    kprintf("PID: %d\n", currpid);
+    kprintf("CPU time: %d\n", getcputime(currpid));
+    kprintf("priority: %d\n", getprio(currpid));
+    kprintf("preempt: %d\n", preempt);
+}
--- xinu/system/main.c
+++ xinu/system/main.c
@@ -1,31 +1,44 @@
 /*  main.c  - main */
 
 #include <xinu.h>
+#include <stdio.h>
+
+extern void cpubound(void);
+extern void iobound(void);
 
 process	main(void)
 {
-    
-    	kprintf("\nHello World!\n");
-    	kprintf("\nI'm the first XINU app and running function main() in system/main.c.\n");
-    	kprintf("\nI was created by nulluser() in system/initialize.c using create().\n");
-    	kprintf("\nMy creator will turn itself into the do-nothing null process.\n");
-    	kprintf("\nI will create a second XINU app that runs shell() in shell/shell.c as an example.\n");
-    	kprintf("\nYou can do something else, or do nothing; it's completely up to you.\n");
-    	kprintf("\n...creating a shell\n");
+	kprintf("\nHello World!\n");
 
-	/* Run the Xinu shell */
+        uint32 start_time = clktime;
 
 	recvclr();
-	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
 
-	/* Wait for shell to exit and recreate it */
+        resume(create((void*) cpubound, 1024, 50, "cpubound", 0, NULL));
+        resume(create((void*) cpubound, 1024, 50, "cpubound", 0, NULL));
+        resume(create((void*) cpubound, 1024, 50, "cpubound", 0, NULL));
+        resume(create((void*) cpubound, 1024, 50, "cpubound", 0, NULL));
+        resume(create((void*) cpubound, 1024, 50, "cpubound", 0, NULL));
+
+        resume(create((void*) iobound, 1024, 50, "iobound", 0, NULL));
+        resume(create((void*) iobound, 1024, 50, "iobound", 0, NULL));
+        resume(create((void*) iobound, 1024, 50, "iobound", 0, NULL));
+        resume(create((void*) iobound, 1024, 50, "iobound", 0, NULL));
+        resume(create((void*) iobound, 1024, 50, "iobound", 0, NULL));
+
+        uint32 end_time = clktime;
+
+        kprintf("\nTotal Simulation Time: %d\n", end_time - start_time);
+
+        resume(create(shell, INITSTK, 50, "shell", 1, CONSOLE));
+
+        kill(currpid);
+
+	// Wait for shell to exit and recreate it
 
 	while (TRUE) {
 		receive();
-		sleepms(200);
-		kprintf("\n\nMain process recreating shell\n\n");
-		resume(create(shell, 4096, 20, "shell", 1, CONSOLE));
+		// resume(create(shell, 4096, 20, "shell", 1, CONSOLE));
 	}
 	return OK;
-    
 }
